af744fe254121303a4448e173dd640648637db0b
Your Name <you@example.com>
2025-10-01 00:05:38 +0500

    wdwdввddвdd

--- service/index.ts
+++ service/index.ts
@@ -1,28 +1,23 @@
 const express = require('express');
 const app = express();
-const port = 5422
+const port = 5422;
 const cors = require("cors");
-
 
 app.use(cors());
 app.use(express.json());
 
-
-app.get("/random", (req: Request, res: Response) => {
+app.get("/random", (req, res) => {
     const random = Math.floor(Math.random() * 1000);
-    //@ts-ignore
-    return res.status(200).send(random);
-})
+    return res.status(200).send(random.toString());
+});
 
-
-app.get("/random/math", (req: Request, res: Response) => {
+app.get("/random/math", (req, res) => {
     const random = Math.floor(Math.random() * 1000);
     const random2 = Math.floor(Math.random() * 1000);
-    const result = random + random2
-    //@ts-ignore
-    return res.status(200).send(result);
-})
+    const result = random + random2;
+    return res.status(200).send(result.toString());
+});
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server started on port ${port}`);
-})
+});
